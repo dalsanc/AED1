55622dd86e4be86f10be857fb73f25849a9b0655
dalsanc <165969508+dalsanc@users.noreply.github.com>
2025-09-23 21:40:06 -0300

    Add files via upload

---
+++ 1180a.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int X[1000];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &X[i]);
+    }
+
+    int menor = X[0];
+    int pos = 0;
+
+    for (int i = 1; i < N; i++) {
+        if (X[i] < menor) {
+            menor = X[i];
+            pos = i;
+        }
+    }
+
+    printf("Menor valor: %d\n", menor);
+    printf("Posicao: %d\n", pos);
+
+    return 0;
+}
