1354be43bfefd8aa5d83f61e5298418476b976e2
dalsanc <165969508+dalsanc@users.noreply.github.com>
2025-12-22 11:16:19 -0300

    Update 1062.c

--- 1062.c
+++ 1062.c
@@ -2,37 +2,39 @@
 
 int main() {
     int n;
+
     while (scanf("%d", &n) && n != 0) {
         int sequencia[1000];
-        
-    while (1) {
+
+        while (1) {
             scanf("%d", &sequencia[0]);
+
             if (sequencia[0] == 0) {
                 printf("\n");
                 break;
             }
-            
-        for (int i = 1; i < n; i++) {
+
+            for (int i = 1; i < n; i++) {
                 scanf("%d", &sequencia[i]);
             }
-            
+
             int pilha[1000], topo = -1;
             int vagao = 1;
             int possivel = 1;
-            
-        for (int i = 0; i < n; i++) {
-            while (vagao <= sequencia[i]) {
+
+            for (int i = 0; i < n; i++) {
+                while (vagao <= sequencia[i]) {
                     pilha[++topo] = vagao++;
                 }
-                
-            if (topo >= 0 && pilha[topo] == sequencia[i]) {
+
+                if (topo >= 0 && pilha[topo] == sequencia[i]) {
                     topo--;
                 } else {
                     possivel = 0;
                     break;
                 }
             }
-            
+
             if (possivel) {
                 printf("Yes\n");
             } else {
@@ -40,5 +42,6 @@ int main() {
             }
         }
     }
+
     return 0;
 }
